67f23cb9586e73c167a166606746e97398799b8d
Alex Wen <ayw19@lx00.hep.ph.ic.ac.uk>
2019-07-09 12:16:46 +0100

    Updates to various helper scripts

---
+++ energytesttest.sh
@@ -0,0 +1,3 @@
+nohup ./energy_test data/trunc_sample1.txt data/trunc_sample2.txt --n-permutations=5 --delta-value=0.4 >> nohup1.out 2>&1& 
+nohup ./energy_test data/trunc_sample1.txt data/trunc_sample2.txt --n-permutations=5 --delta-value=0.45 >> nohup2.out 2>&1& 
+nohup ./energy_test data/trunc_sample1.txt data/trunc_sample2.txt --n-permutations=5 --delta-value=0.5 >> nohup3.out 2>&1& 
